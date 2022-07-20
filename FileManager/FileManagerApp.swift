902b485a684d9f9009d04f9422e99e57b109356b
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-20 08:22:36 +0200

    Initial Commit

---
+++ FileManager/FileManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerApp.swift
+//  FileManager
+//
+//  Created by Skorobogatow, Christian on 20/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
